a980eb3005e57dcee241254e54ae77d55605a109
Abel <abel99ms@gmail.com>
2024-04-29 12:17:32 +0200

    first commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyC66LnvlZIKrbcBZK8Ecz9Gl0sr6jCa5oU",
+  authDomain: "projectefirebase-c5ba4.firebaseapp.com",
+  projectId: "projectefirebase-c5ba4",
+  storageBucket: "projectefirebase-c5ba4.appspot.com",
+  messagingSenderId: "1031297841789",
+  appId: "1:1031297841789:web:01fb9acc066419a9890f14",
+  measurementId: "G-SNG88BP4MV"
+});
+
+const auth = firebase.auth();
